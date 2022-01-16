f02180423d38dfb35245f44d13328d3d728d9614
hocku <ted221997@gmail.com>
2022-01-16 21:47:20 +0300

    front side

--- testsite/src/App.js
+++ testsite/src/App.js
@@ -1,10 +1,30 @@
+import React,{useState} from "react"
+import { Form, Button } from "react-bootstrap";
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [login,setLogin]=useState('')
+  const [password,setPassword]=useState('')
+  const onSubmitForm = () => {
+    console.log(login+","+password)
+  }
   return (
     <div className="App">
-    
+      <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={login} onChange={({target})=>setLogin(target.value)}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={({target})=>setPassword(target.value)}/>
+        </Form.Group>
+          <Button variant="primary" onClick={onSubmitForm}>
+            Submit
+          </Button>
+      </Form>
     </div>
   );
 }
